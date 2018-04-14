c237a5b6ba7c7aa582d78472fe91c8babc164aef
王泽中 <janebenjohn@outlook.com>
2018-04-14 16:38:02 +0800

    删掉无用的注释

--- groceryHouse/src/router/index.js
+++ groceryHouse/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import origin from '@/components/origin'
 
 Vue.use(Router)
 
@@ -9,8 +9,12 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'origin',
+      component: origin
+    },
+    {
+      path: '*',
+      redirect: '/', // 重定向
     }
   ]
 })
