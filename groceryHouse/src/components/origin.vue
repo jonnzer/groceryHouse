59c0b54a4554b033724667174c244eb14ef56845
王泽中 <janebenjohn@outlook.com>
2018-04-15 13:58:39 +0800

    引入mojs动画库，并在 起始vue内引入

--- groceryHouse/src/components/origin.vue
+++ groceryHouse/src/components/origin.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="hello">
-    origin page
   </div>
 </template>
 
@@ -11,7 +10,27 @@ export default {
     return {
      
     };
-  }
+  },
+  created(){
+    this.originAct()
+  },
+  methods: {
+    originAct() {
+        const rect = new mojs.Shape({
+          shape:  			'rect',
+          left:         '50%',
+          fill:         'none',
+          radius:       20,
+          stroke:       { 'rgba(0,255,255, 1)' : 'magenta' },
+          strokeWidth:  { 10: 0 },
+          strokeDasharray: '100%',
+          strokeDashoffset: { '-100%' : '100%' },
+          angle:        { 0: 180 },
+          duration:     2000,
+          repeat:       999,
+        }).play();
+    }
+  },
 };
 </script>
 
--- groceryHouse/src/main.js
+++ groceryHouse/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import moJs from 'mo-js'
 
 Vue.config.productionTip = false
+Vue.use(moJs)
 
 /* eslint-disable no-new */
 new Vue({
