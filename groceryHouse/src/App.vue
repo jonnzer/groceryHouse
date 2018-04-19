9f2112ae65f3914203c4db1b203037ca14250eb3
王泽中 <janebenjohn@outlook.com>
2018-04-19 16:00:25 +0800

    修改部分样式，让demo满屏

--- groceryHouse/src/App.vue
+++ groceryHouse/src/App.vue
@@ -20,7 +20,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background:#ecf0f1;
+  width: 100%;
+  height: 100%;
 }
 
 
