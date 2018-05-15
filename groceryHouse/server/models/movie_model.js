acfe718fbb40ec87157c90fb084890e248f959a0
王泽中 <janebenjohn@outlook.com>
2018-05-15 15:59:45 +0800

    添加连接数据库的用户名和密码(nodejs 的全局常量)。

--- groceryHouse/server/models/movie_model.js
+++ groceryHouse/server/models/movie_model.js
@@ -1,2 +1,6 @@
-import groceryHouse from '../config/db'
-console.log(groceryHouse)
+import db from '../config/db.js'
+const movieModel = '../schema/grocery_movie.js'
+const movieDb = db.groceryHouse     // 引入数据库
+const instance_movie = movieDb.import(movieModel) // 引入表结构，实例化
+console.log(db)
+
