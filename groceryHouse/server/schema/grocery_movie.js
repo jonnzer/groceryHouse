91fea16b9d9d43d3b716d48685eac68319e2e2ce
王泽中 <janebenjohn@outlook.com>
2018-05-15 14:45:32 +0800

    sqelize-autl自动导出的数据结构

---
+++ groceryHouse/server/schema/grocery_movie.js
@@ -0,0 +1,53 @@
+/* jshint indent:
+      sequelize-auto -o './schema' -d groceryHouse -h 127.0.0.1 -u root -p 3306 -x 123456 -e mysql;
+
+2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('grocery_movie', {
+    movie_id: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    movie_create_data: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    movie_name: {
+      type: DataTypes.CHAR(255),
+      allowNull: false
+    },
+    movie_actor: {
+      type: DataTypes.CHAR(255),
+      allowNull: false
+    },
+    movie_tag: {
+      type: DataTypes.CHAR(255),
+      allowNull: false
+    },
+    movie_sum: {
+      type: DataTypes.CHAR(255),
+      allowNull: false
+    },
+    movie_high_pic: {
+      type: DataTypes.CHAR(255),
+      allowNull: false
+    },
+    movie_simple_pic: {
+      type: DataTypes.CHAR(255),
+      allowNull: false
+    },
+    movie_content: {
+      type: DataTypes.CHAR(255),
+      allowNull: false
+    },
+    movie_seed: {
+      type: DataTypes.CHAR(255),
+      allowNull: false
+    }
+  }, {
+    tableName: 'grocery_movie'
+  });
+};
