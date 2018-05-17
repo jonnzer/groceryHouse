5fd0023aef7cc4e9357d428c63527d9e40067126
王泽中 <janebenjohn@outlook.com>
2018-05-17 21:13:53 +0800

    电影的数据层（model）以及控制器层（contrller）的初步demo

--- groceryHouse/server/controllers/movie_ctl.js
+++ groceryHouse/server/controllers/movie_ctl.js
@@ -1,6 +1,14 @@
 import instance_movie from '../models/movie_model'
 
 const getNewMovie = async function (ctx) {
-
-    ctx.body = 123
+    
+    ctx.body = {
+        success: true,
+        content: '213'
+    }
+    
 } 
+
+export default {
+    getNewMovie,
+}
--- groceryHouse/server/models/movie_model.js
+++ groceryHouse/server/models/movie_model.js
@@ -2,5 +2,6 @@ import db from '../config/db.js'
 const movieModel = '../schema/grocery_movie.js'
 const movieDb = db.groceryHouse     // 引入数据库
 const instance_movie = movieDb.import(movieModel) // 引入表结构，实例化
-console.log(db)
+
+// model 
 
