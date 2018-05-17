4df3de68594573b37147cb89b3d73902f8275d76
王泽中 <janebenjohn@outlook.com>
2018-05-17 21:15:35 +0800

    修改数据库引入的env路径。

--- groceryHouse/server/config/db.js
+++ groceryHouse/server/config/db.js
@@ -1,8 +1,7 @@
 // db.js: 初始化和数据库的连接
 
-import '../../.env'
-
-const Sequelize = require('sequelize')
+import '../../env'
+import Sequelize from 'sequelize'
 
 const groceryHouse = new Sequelize(`mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@localhost/groceryHouse`,{
   define: {
