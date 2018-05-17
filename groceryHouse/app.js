f0cebb25aa8fad05afa7ce2fd01722070f208f44
王泽中 <janebenjohn@outlook.com>
2018-05-17 21:15:09 +0800

    修改koa的入口文件，导入电影路由，添加到koa路由，并且监听env的端口。

--- groceryHouse/app.js
+++ groceryHouse/app.js
@@ -1,9 +1,37 @@
 // koa入口文件
-const Koa = require('koa');
+import './env'
+import Koa from 'koa'
+import koaRouter from 'koa-router'
+import movie_route from './server/routes/movie_route'
+// import json from 'koa-json'
+// import koaBodyparser from 'koa-bodyparser'
+
 const app = new Koa();
+const router = koaRouter()
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
+let port = process.env.PORT
+
+// app.use(koaBodyparser())
+// app.use(json())
+
+app.use(async function (ctx, next) {
+  let start = new Date()
+  await next()
+  let ms = new Date() - start
+  console.log('%s %s - %s', ctx.method, ctx.url, ms)
+})
+
+app.on('error', function (err, ctx) {
+  console.log('server error', err)
+})
+
+router.use('/movie', movie_route.routes()) // 电影api
+router.get('/caidan', function (ctx, next) {
+	ctx.body = 'Hello caidan!';
 });
+app.use(router.routes())
+export default app.listen(port, () => {
+  console.log(`Koa is listening in ${port}`)
+})
 
-app.listen(3000);
+
