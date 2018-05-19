2d86308737c2ece2fa478f9ac07c204dd99905e9
王泽中 <janebenjohn@outlook.com>
2018-05-19 18:32:13 +0800

    修补数据库连接时自动创建createAt问题，新建连接数据库状态输出log

--- groceryHouse/server/config/db.js
+++ groceryHouse/server/config/db.js
@@ -5,10 +5,18 @@ import Sequelize from 'sequelize'
 
 const groceryHouse = new Sequelize(`mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@localhost/groceryHouse`,{
   define: {
-    timeStamp: false,
+    timestamps: false,
   }
 })
 
+groceryHouse  
+  .authenticate()
+  .then(() => { // test connect 数据库 状态
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  })
 export default {
   groceryHouse    //  导出为数据库的model
 }
