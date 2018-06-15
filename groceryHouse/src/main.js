b521d37afe47abeb625cd2c7e02adce8590c6c88
王泽中 <janebenjohn@outlook.com>
2018-06-15 14:58:09 +0800

    引入滚动插件

--- groceryHouse/src/main.js
+++ groceryHouse/src/main.js
@@ -3,9 +3,11 @@ import App from './App'
 import router from './router'
 import moJs from 'mo-js'
 import store from './vuex'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 
 Vue.config.productionTip = false
 Vue.use(moJs)
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
