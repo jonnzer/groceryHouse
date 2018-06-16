ebb63f11ab6c93757a7535f5d98dfbbf1e0c0784
王泽中 <janebenjohn@outlook.com>
2018-06-16 15:22:16 +0800

    解决前端页面刷新丢失问题。

--- groceryHouse/app.js
+++ groceryHouse/app.js
@@ -11,6 +11,8 @@ import comic_route from './server/routes/comic_route'
 const app = new Koa();
 const path = require('path')          // nodejs自带
 const serve = require('koa-static')   // 引入静态资源服务器
+const historyApiFallback = require('koa-history-api-fallback');   // 引入依赖，解决做静态服务器后刷新丢失页面
+
 const router = koaRouter()
 let port = process.env.PORT
 
@@ -28,14 +30,16 @@ app.on('error', function (err, ctx) {
   console.log('server error', err)
 })
 
-app.use(serve(path.resolve('dist')))      // 将webpack打包好的项目目录作为Koa静态文件服务的目录
-
 router.use('/movie', movie_route.routes()) // 电影api
 router.use('/comic',comic_route.routes())  // 漫画api
 router.get('/caidan', function (ctx, next) {
 	ctx.body = 'Hello caidan!';
 });
+
 app.use(router.routes())
+app.use(historyApiFallback())
+app.use(serve(path.resolve('dist')))      // 将webpack打包好的项目目录作为Koa静态文件服务的目录
+
 export default app.listen(port, () => {
   console.log(`Koa is listening in ${port}`)
 })
